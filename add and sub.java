d321eebbf1e60d42a6c5fde7c2dbaca2f7fa0b26
thanmai876 <160998752+thanmai876@users.noreply.github.com>
2025-03-04 14:51:54 +0530

    Create add and sub.java

---
+++ add and sub.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        // Define two built-in numbers
+        double num1 = 15.5;
+        double num2 = 5.2;
+        
+        // Perform addition
+        double sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+        
+        // Perform subtraction
+        double difference = num1 - num2;
+        System.out.println("Difference: " + difference);
+    }
+}
